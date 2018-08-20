5b78e3b90754a7a21dcb8fbd1c523e77604c1878
Simon Erhardt <hello@rootlogin.ch>
2018-08-20 21:23:04 +0200

    use pointers

--- default.go
+++ default.go
@@ -4,33 +4,33 @@ type GoWikiPlugin struct {
 	routes map[string]func (request HTTPRequest) string
 }
 
-func (r GoWikiPlugin) Routes() []string {
-	if r.routes == nil {
+func (p *GoWikiPlugin) Routes() []string {
+	if p.routes == nil {
 		return []string{}
 	}
 
 	var routes []string
-	for k, _ := range r.routes {
+	for k, _ := range p.routes {
 		routes = append(routes, k)
 	}
 
 	return routes
 }
 
-func (r GoWikiPlugin) RegisterRoute(route string, handler func (HTTPRequest) string) {
-	if r.routes == nil {
-		r.routes = make(map[string]func (request HTTPRequest) string)
+func (p *GoWikiPlugin) RegisterRoute(route string, handler func (HTTPRequest) string) {
+	if p.routes == nil {
+		p.routes = make(map[string]func (request HTTPRequest) string)
 	}
 
-	r.routes[route] = handler
+	p.routes[route] = handler
 }
 
-func (r GoWikiPlugin) HandleRoute(route string, request HTTPRequest) string {
-	if r.routes == nil {
+func (p *GoWikiPlugin) HandleRoute(route string, request HTTPRequest) string {
+	if p.routes == nil {
 		return "Internal server error"
 	}
 
-	if routeFunc, ok := r.routes[route]; ok {
+	if routeFunc, ok := p.routes[route]; ok {
 		return routeFunc(request)
 	}
 
